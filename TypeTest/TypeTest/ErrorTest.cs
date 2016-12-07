9bcce3eada2089fa05e1d6dfaca127a0f5adb94e
MythZ <zen1225@gmail.com>
2016-12-08 00:32:44 +0800

    [MZ]add

---
+++ TypeTest/TypeTest/ErrorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTest
+{
+    class ErrorTest
+    {
+        public int MyMethod(bool YesNo)
+        {
+            if (YesNo)
+            {
+                return 45;
+            }
+            else
+            {
+                return 61;
+            }
+        }
+    }
+}
---
+++ TypeTest/TypeTest/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypeTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ErrorTest myTest = new ErrorTest();
+            myTest.MyMethod(true);
+
+            MessageBox.Show();
+
+            /*
+            int myInt=10;
+            byte mybyte = (byte)myInt;
+            double mydouble = (double)mybyte;
+            bool myBool = (bool)mydouble;
+            string myString = "false";
+            myBool = (bool)myString;
+            myString = (string)myInt;
+            myString = myInt.ToString();
+            myBool = (bool)mybyte;
+            mybyte = (byte)myBool;
+            short myShort = (short)myInt;
+            char myChar = 'x';
+            myString = (string)myChar;
+            long myLong = (long)myInt;
+            decimal myDecimal = (decimal)myLong;
+            myString = myString + myInt + mybyte + myChar+mydouble;
+             */
+
+        }
+    }
+}
