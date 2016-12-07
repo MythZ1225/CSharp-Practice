d052b056f5bd528bbc2ac4ae99f6b23fa9a27a69
MythZ <zen1225@gmail.com>
2016-12-08 00:33:15 +0800

    [MZ]Add

---
+++ MileageCalculator/MileageCalculator/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MileageCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartingMileageNum = new System.Windows.Forms.NumericUpDown();
+            this.EndingMileageNum = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.StartingMileageNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EndingMileageNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Mileage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ending Mileage";
+            // 
+            // StartingMileageNum
+            // 
+            this.StartingMileageNum.Location = new System.Drawing.Point(112, 15);
+            this.StartingMileageNum.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.StartingMileageNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.StartingMileageNum.Name = "StartingMileageNum";
+            this.StartingMileageNum.Size = new System.Drawing.Size(176, 22);
+            this.StartingMileageNum.TabIndex = 2;
+            this.StartingMileageNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // EndingMileageNum
+            // 
+            this.EndingMileageNum.Location = new System.Drawing.Point(112, 43);
+            this.EndingMileageNum.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.EndingMileageNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.EndingMileageNum.Name = "EndingMileageNum";
+            this.EndingMileageNum.Size = new System.Drawing.Size(176, 22);
+            this.EndingMileageNum.TabIndex = 3;
+            this.EndingMileageNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Amount Ownd ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(110, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(110, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Miles Traveled";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 177);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EndingMileageNum);
+            this.Controls.Add(this.StartingMileageNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "里程計算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StartingMileageNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EndingMileageNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown StartingMileageNum;
+        private System.Windows.Forms.NumericUpDown EndingMileageNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MileageCalculator/MileageCalculator/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MileageCalculator
+{
+    public partial class Form1 : Form
+    {
+        int statingMileage, endingMileage;
+        double mileTraveled;
+        double reimburseRate = 0.39F;
+        double amountOwed = 0D;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Text = 0 + " miles";
+            label4.Text = "$" + 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            statingMileage = (int)StartingMileageNum.Value;
+            endingMileage = (int)EndingMileageNum.Value;
+            if (endingMileage >= statingMileage)
+            {
+                mileTraveled = endingMileage - statingMileage;
+                amountOwed = mileTraveled * reimburseRate;
+                label4.Text = "$" + amountOwed.ToString("0.00");
+                label5.Text = mileTraveled + " miles";
+            }
+            else
+            {
+                MessageBox.Show("結束里程需大於開始里程", "無法計算");
+            }
+            
+        }
+       
+    }
+}
