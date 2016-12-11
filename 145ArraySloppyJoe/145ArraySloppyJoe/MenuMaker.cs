9971762f37d8328e7614270c0bdaad4dacb60a49
MythZ <zen1225@gmail.com>
2016-12-12 02:49:51 +0800

    [MZ]Add

---
+++ 145ArraySloppyJoe/145ArraySloppyJoe/MenuMaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _145ArraySloppyJoe
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+        string[] Meats = {"烤牛肉","沙拉","火腿","火雞肉" };
+        string[] Condiments = { "芥末", "番茄醬", "蜂蜜", "美乃滋" };
+        string[] Breads = { "白麵包", "燕麥麵包", "胚芽麵包", "黑麵包" };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiments = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBreads = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat+ randomCondiments+ randomBreads;
+        }
+
+    }
+
+
+
+}
