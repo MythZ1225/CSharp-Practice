641cf4b623126180fbdc1bba307b97ac2779ff42
MythZ <zen1225@gmail.com>
2017-03-14 04:26:43 +0800

    [MZ]add

---
+++ 272 Vertebrate/272 Vertebrate/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _272_Vertebrate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bird tweety = new bird();
+            tweety.WingSpan = 7.5;
+            tweety.fly();
+            tweety.Eat();
+            tweety.NumOfLegs = 2;
+
+
+        }
+    }
+}
---
+++ 272 Vertebrate/272 Vertebrate/Vertbrate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _272_Vertebrate
+{
+    class Vertbrate
+    {
+        public int NumOfLegs;
+        public void Eat()
+        {
+            //code to eat
+        }
+    }
+
+    class bird : Vertbrate
+    {
+        public double WingSpan;
+        public void fly()
+        {
+            //code to fly
+        }
+    }
+}
