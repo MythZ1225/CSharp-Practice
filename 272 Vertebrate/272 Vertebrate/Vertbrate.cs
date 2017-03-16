2a082ea27c67fec9f18a274fbf53ca5ebf647c86
MythZ <zen1225@gmail.com>
2017-03-17 01:34:05 +0800

    [MZ]Update

--- 272 Vertebrate/272 Vertebrate/Vertbrate.cs
+++ 272 Vertebrate/272 Vertebrate/Vertbrate.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace _272_Vertebrate
 {
+    public class Food
+    {
+    }
+
     class Vertbrate
     {
-        public int NumOfLegs;
-        public void Eat()
+        public int NumOfLegs;        
+        public virtual void Eat(Food Morsel)
         {
-            //code to eat
+            Swallow(Morsel);
+            Digest();
         }
+        public void Swallow(Food Morsel) { }
+        public void Digest() { }
     }
 
     class bird : Vertbrate
@@ -23,4 +30,17 @@ namespace _272_Vertebrate
             //code to fly
         }
     }
+
+    class Chameleon : Vertbrate
+    {
+        public override void Eat(Food Morsel)
+        {
+            catchWithTongue(Morsel);
+            base.Eat(Morsel);
+        }
+        private void catchWithTongue(Food Morsel ) { }
+        
+    }
+
+
 }
