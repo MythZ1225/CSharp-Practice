847aaf4e09fded420da59d924581fcfa659742e6
MythZ <zen1225@gmail.com>
2017-03-18 17:58:48 +0800

    [MZ]add

---
+++ 314TallGuyInterFaceTest1/314TallGuyInterFaceTest1/TallGuy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _314TallGuyInterFaceTest1
+{
+    class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            MessageBox.Show("我叫" + Name + " 我身高" + Height + "公分");
+        }
+
+        public string FunnyThingIHave
+        {
+            get { return "大鞋"; }
+        }
+
+        public void Honk()
+           {
+            MessageBox.Show("齁齁");
+           }
+
+    }
+}
