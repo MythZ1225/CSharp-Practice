40c6fced53f776a42ba910a99b4bc66547bd5e4d
MythZ <zen1225@gmail.com>
2017-04-04 01:40:27 +0800

    [MZ]add random Card

---
+++ 369Cards_Enum/369Cards_Enum/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _369Cards_Enum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            textBox1.Text = card.Name;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
