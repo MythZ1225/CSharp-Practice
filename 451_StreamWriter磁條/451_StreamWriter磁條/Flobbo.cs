927750f1047a3f48db0811f71e20357a0307ffe0
MythZ <zen1225@gmail.com>
2017-07-26 23:47:19 +0800

    [MZ]Add

---
+++ 451_StreamWriter磁條/451_StreamWriter磁條/Flobbo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _451_StreamWriter磁條
+{
+    class Flobbo
+    {
+        private string Zap;
+
+        public Flobbo(string Zap)
+        {
+            this.Zap = Zap;
+        }
+
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter(@"C:\macaw.txt");
+        }
+
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(Zap);
+            Zap = "綠紫";
+            return false;
+
+        }
+
+        public bool Blobbo(bool Already, StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(Zap);
+                sw.Close();
+                return false;
+
+            }
+            else
+            {
+                sw.WriteLine(Zap);
+                Zap = "紅橘";
+                return true;
+            }
+        }
+
+    }
+}
