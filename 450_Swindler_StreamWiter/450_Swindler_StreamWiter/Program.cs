e215a88b47d0a69fd952d5b55774aa01b1175379
MythZ <zen1225@gmail.com>
2017-07-26 23:20:21 +0800

    [MZ]add

---
+++ 450_Swindler_StreamWiter/450_Swindler_StreamWiter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _450_Swindler_StreamWiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\secret_plan.txt");
+
+            sw.WriteLine("如何打敗超人");
+            sw.WriteLine("我另一個絕妙的計畫");  //WriteLine 會換行
+            sw.Write("我會創造一群複製人大軍且");      //Write 不會換行
+            sw.WriteLine("放出他們到城裡作亂");
+
+            string location = "商場";
+
+            for (int i = 0; i <= 6; i++)
+            {
+                sw.WriteLine("複製人{0}號 攻擊{1}", i, location);
+
+                if (location == "商場")
+                {
+                    location = "市中心";
+                }
+                else
+                {
+                    location = "商場";
+                }
+            }
+
+            sw.Close();
+        }
+    }
+}
