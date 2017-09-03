f1124fb8e40a58bb3ec5dca9fbbc06926a5d731e
MythZ <zen1225@gmail.com>
2017-09-03 15:48:50 +0800

    [MZ]add

---
+++ 335_ PlantMission_Abstract/335_ PlantMission_Abstract/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _335__PlantMission_Abstract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // PlanetMission PM = new PlanetMission();
+
+            Venus VM = new Venus();
+
+            Mars MM = new Mars();
+
+            // MessageBox.Show("PM" + PM.FuelNeeded());
+            MessageBox.Show("VM" + VM.FuelNeeded());
+            MessageBox.Show("MM" + MM.FuelNeeded());
+        }
+    }
+}
---
+++ 335_ PlantMission_Abstract/335_ PlantMission_Abstract/PlanetMission.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _335__PlantMission_Abstract
+{
+    abstract class PlanetMission
+    {
+        public long RocketFuelPerMile;
+        public long RockeSpeedMPH;
+        public int MilesToPlanet;
+
+        public long UnitOfFelNeeded()
+        {
+            return MilesToPlanet * RocketFuelPerMile;
+        }
+
+        public int TimeNeeded()
+        {
+            return MilesToPlanet / (int)RockeSpeedMPH;
+        }
+
+        public string FuelNeeded()
+        {
+            return "你需要" + UnitOfFelNeeded() + "單位的油" + TimeNeeded() + "小時，才會到達目的地。";
+        }
+
+        public abstract void SetMissionInfo(int milesToPlanet, int rocketFuelPerMile, long rockeSpeedMPH);
+        
+    }
+
+    class Venus : PlanetMission
+    {
+        public Venus()
+        {
+            MilesToPlanet = 4000000;
+            RockeSpeedMPH = 25000;
+            RocketFuelPerMile = 100000;
+        }
+
+        public override void SetMissionInfo(int milesToPlanet, int rocketFuelPerMile, long rockeSpeedMPH)
+        {
+            this.MilesToPlanet = milesToPlanet;
+            this.RockeSpeedMPH = rockeSpeedMPH;
+            this.RocketFuelPerMile = rocketFuelPerMile;
+        }
+    }
+
+    class Mars : PlanetMission
+    {
+        public Mars()
+        {
+            MilesToPlanet = 7500000;
+            RockeSpeedMPH = 25000;
+            RocketFuelPerMile = 100000;
+        }
+
+        public override void SetMissionInfo(int milesToPlanet, int rocketFuelPerMile, long rockeSpeedMPH)
+        {
+            this.MilesToPlanet = milesToPlanet;
+            this.RockeSpeedMPH = rockeSpeedMPH;
+            this.RocketFuelPerMile = rocketFuelPerMile;
+        }
+    }
+}
