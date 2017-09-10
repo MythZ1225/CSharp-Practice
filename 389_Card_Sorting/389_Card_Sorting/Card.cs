c44fae928ea3ed3b4f2f095c62be586d63e1eb87
MythZ <zen1225@gmail.com>
2017-09-10 18:32:07 +0800

    [MZ]Add

---
+++ 389_Card_Sorting/389_Card_Sorting/Card.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _389_Card_Sorting
+{
+
+        enum Suits {
+            黑桃,
+            梅花,
+            紅心,
+            鑽石,
+        }
+
+        enum Values {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            J = 11,
+            Q = 12,
+            K = 13,
+        }
+
+    class Card
+    {
+        public Suits suit{get;set;}
+        public Values value { get; set; }
+
+        public Random random = new Random();
+
+        public Card(Suits suit, Values value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+        public string CardName
+        {
+           get { return suit.ToString() + value.ToString(); }
+        }
+    }
+}
---
+++ 389_Card_Sorting/389_Card_Sorting/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _389_Card_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+            Random random = new Random();
+            Card card1 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card2 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card3 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card4 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card5 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card6 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card7 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card8 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card9 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            Card card10 = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+
+            cards.Add(card1);
+            cards.Add(card2);
+            cards.Add(card3);
+            cards.Add(card4);
+            cards.Add(card5);
+            cards.Add(card6);
+            cards.Add(card7);
+            cards.Add(card8);
+            cards.Add(card9);
+            cards.Add(card10);
+
+            CardComparer cardComparer = new CardComparer();
+
+            PrintCards(cards);
+            Console.ReadKey();
+
+            cards.Sort(cardComparer);
+            PrintCards(cards);
+            Console.ReadKey();
+
+            cardComparer.sorttype = SortType.ValueThanSuit;
+            cards.Sort(cardComparer);
+            PrintCards(cards);
+            Console.ReadKey();
+
+
+        }
+
+        public static void PrintCards(List<Card> cards)
+        {
+
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.CardName);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
