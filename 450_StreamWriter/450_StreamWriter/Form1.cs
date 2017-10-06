c16e10f6fafc9e8107b90a59a2be923814d231f7
MythZ <zen1225@gmail.com>
2017-10-07 00:53:30 +0800

    [MZ]Update

--- 450_StreamWriter/450_StreamWriter/Form1.cs
+++ 450_StreamWriter/450_StreamWriter/Form1.cs
@@ -13,20 +13,22 @@ namespace _450_StreamWriter
 {
     public partial class Form1 : Form
     {
-        StreamWriter sw = new StreamWriter(@"L:\給超人的信2.txt");
+        StreamReader sr = new StreamReader(@"L:\給超人的信2.txt");
+
+        StreamWriter sw = new StreamWriter(@"L:\給超人的信3.txt");
         
 
         public Form1()
         {
             InitializeComponent();
 
-            sw.WriteLine("如何打敗超人");
-            sw.WriteLine("其他邪惡計畫");
-            sw.Write("製作更多邪惡怪人");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            sw.WriteLine("如何打敗超人");
+            sw.WriteLine("其他邪惡計畫");
+            sw.Write("製作更多邪惡怪人");
             string Location = "購物城";
             for (int i = 0; i < 6; i++)
             {
@@ -41,6 +43,24 @@ namespace _450_StreamWriter
                 }
             }
             sw.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sw.WriteLine("給 超人");
+            sw.WriteLine("來自 邪惡博士");
+            sw.WriteLine("你阻擋不了我的!");
+            sw.WriteLine();
+            sw.WriteLine("我發現了大秘密!");
+            while (!sr.EndOfStream)     
+            {
+                string lineFromThePlan = sr.ReadLine();
+                sw.WriteLine("計畫→" + lineFromThePlan);
+            }
+            sw.WriteLine();
+            sw.WriteLine("救命啊!");
+            sw.Close();
+            sr.Close();
         }
     }
 }
