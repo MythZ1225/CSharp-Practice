e71b7580c6a158d72bf79d0b37545b05f53fb837
MythZ <zen1225@gmail.com>
2017-10-09 23:12:07 +0800

    add

---
+++ 485_EurekaUnicode/485_EurekaUnicode/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _485_EurekaUnicode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("eureka.txt", "Eureka!");
+            listBox1.Items.Add("Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach (byte b in eurekaBytes)
+            {
+                listBox1.Items.Add(b);
+            }
+            listBox1.Items.Add("\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach (byte b in eurekaBytes)
+            {
+                Console.Write("{0:x2} ",b);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("eureka.txt", "017w", Encoding.Unicode);
+
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach (byte b in eurekaBytes)
+            {
+                listBox1.Items.Add(b);
+            }
+            listBox1.Items.Add("\n");
+        }
+    }
+}
